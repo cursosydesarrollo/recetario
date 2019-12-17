b71af98c9f974b2ad96793a267c5fd73de1be5a3
Gerardo Belot <gbelot2003@hotmail.com>
2019-12-16 19:48:22 -0600

    Configurando archivos para creacion de Crud de usuarios

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -36,6 +36,10 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a href="{{ route('users') }}" class="dropdown-item">{{  __('Usuarios') }}</a> 
+                            </div>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
